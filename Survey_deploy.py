60060d889cc07f25da7f59b084c5b2fc0de88b9d
cbott44 <cb44@ksu.edu>
2025-07-15 10:49:10 -0500

    Update Survey_deploy.py

--- Survey_deploy.py
+++ Survey_deploy.py
@@ -574,7 +574,7 @@ if not st.session_state.form_submitted:
             st.markdown("**Field Location:** *Provide ONE of the following 3 options*")
             
             st.markdown(
-                "<small style='color:gray;'>Identify the specific field. Please be as precise as possible</small>",
+                "<span style='color:#444;font-size:0.95rem, '>Identify the specific field. Please be as precise as possible>",
                 unsafe_allow_html=True
                  )
 
@@ -716,7 +716,7 @@ if not st.session_state.form_submitted:
 
             st.markdown("Describe any events that may have significantly impacted yield")
             st.markdown(
-                "<small style='color:gray;'>e.g. Stripe rust impacted 20% of field</small>",
+                "<span, style='color:gray;font-size:0.95rem'>e.g. Stripe rust impacted 20% of field>",
                 unsafe_allow_html=True
                  )
             new_data2['impacting_events'] = st.text_area("", height = 68)
