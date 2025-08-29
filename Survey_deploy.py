d826bc3d6a008f53f196e05dfdcfc1d40b1d2eb3
cbott44 <cb44@ksu.edu>
2025-08-29 12:51:55 -0500

    Update Survey_deploy.py

--- Survey_deploy.py
+++ Survey_deploy.py
@@ -727,10 +727,10 @@ if not st.session_state.form_submitted:
         
             st.markdown("**List ALL inputs applied in each phase of the season** *(Fertilizers, Fungicides, Herbicides, Pesticides etc)*")
             
-            st.markdown("<small style='color:black;'Pre-Plant/At Seeding'</small>",
-                unsafe_allow_html=True)
+            st.markdown("<small style='color:black;'>Pre-Plant/At Seeding</small>", unsafe_allow_html=True)
+            
+            new_data2["preplant"] = st.text_area("Include product, rate, and time of application for each input")
 
-            new_data2["preplant"]= text.input("Include product, rate, and time of application for each input")        
 
             # st.markdown(
             # "<small style='color:black;'>Fungicide Use? (if yes...)</small>",
